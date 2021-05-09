e78fff9a9bda521fd7ee0693975c21949557a3a8
lalesoyupek <soyupekl@gmail.com>
2021-05-09 12:39:51 +0300

    hello word

---
+++ Introduction/Lesson 1/python.py
@@ -0,0 +1,7 @@
+# yorum satırı, bu alan derlenmeyecek
+
+# alt + z -> wordwarp -> satırı alta kaydır.
+
+print("Hello Word")
+# yaptıgı işin sonucunda bana bırsey döndürüyorsa metot-fonks. pembe kutu çıkar.
+
